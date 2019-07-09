7ac88f595c44fac9552f8e195a1807883cf7b559
Juan Gomez <j.gomez@drexed.com>
2019-07-09 16:04:42 -0400

    Init commit

---
+++ spec/lite/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Lite::Ruby do
+  it "has a version number" do
+    expect(Lite::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
