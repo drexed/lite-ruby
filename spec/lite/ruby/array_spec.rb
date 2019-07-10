6128e6218a3289e4872f7d0ff80127587decf94b
Juan Gomez <j.gomez@drexed.com>
2019-07-10 17:25:31 -0400

    Rework array specs

--- spec/lite/ruby/array_spec.rb
+++ spec/lite/ruby/array_spec.rb
@@ -5,22 +5,25 @@ require 'spec_helper'
 RSpec.describe Array do
 
   describe '#after' do
+    a1 = [1, 2, 3]
     it 'to be 3' do
-      expect([1, 2, 3].after(2)).to eq(3)
+      expect(a1.after(2)).to eq(3)
     end
 
     it 'to be 1' do
-      expect([1, 2, 3].after(3)).to eq(1)
+      expect(a1.after(3)).to eq(1)
     end
 
     it 'to be nil' do
-      expect([1, 2, 3].after(4)).to eq(nil)
+      expect(a1.after(4)).to eq(nil)
     end
   end
 
   describe '#bury' do
+    a1 = [:a1, { b1: :c1 }]
+
     it 'to be [:a1, { b1: :d1 }]' do
-      expect([:a1, { b1: :c1 }].bury(1, :b1, :d1)).to eq([:a1, { b1: :d1 }])
+      expect(a1.bury(1, :b1, :d1)).to eq([:a1, { b1: :d1 }])
     end
 
     it 'to be [:a1, [:c2]]' do
@@ -28,73 +31,93 @@ RSpec.describe Array do
     end
 
     it 'to raise error' do
-      expect { [:a1, { b1: :c1 }].bury(:moo) }.to raise_error(ArgumentError)
+      expect { a1.bury(:moo) }.to raise_error(ArgumentError)
     end
   end
 
   describe '#before' do
+    a1 = [1, 2, 3]
+
     it 'to be 3' do
-      expect([1, 2, 3].before(2)).to eq(1)
+      expect(a1.before(2)).to eq(1)
     end
 
     it 'to be 1' do
-      expect([1, 2, 3].before(1)).to eq(3)
+      expect(a1.before(1)).to eq(3)
     end
 
     it 'to be nil' do
-      expect([1, 2, 3].before(4)).to eq(nil)
+      expect(a1.before(4)).to eq(nil)
     end
   end
 
   describe '#demote(!)' do
+    let(:a1) { [1, 2, 2, 3] }
+
     it 'to be [1, 3, 2, 2]' do
-      expect([1, 2, 2, 3].demote(2)).to eq([1, 3, 2, 2])
-      expect([1, 2, 2, 3].demote!(2)).to eq([1, 3, 2, 2])
+      a2 = [1, 3, 2, 2]
+
+      expect(a1.demote(2)).to eq(a2)
+      expect(a1.demote!(2)).to eq(a2)
     end
 
     it 'to be [2, 2, 3, 1]' do
-      expect([1, 2, 2, 3].demote(1)).to eq([2, 2, 3, 1])
-      expect([1, 2, 2, 3].demote!(1)).to eq([2, 2, 3, 1])
+      a2 = [2, 2, 3, 1]
+
+      expect(a1.demote(1)).to eq(a2)
+      expect(a1.demote!(1)).to eq(a2)
     end
 
     it 'to be [1, 2, 2, 3]' do
-      expect([1, 2, 2, 3].demote(4)).to eq([1, 2, 2, 3])
-      expect([1, 2, 2, 3].demote!(4)).to eq([1, 2, 2, 3])
+      expect(a1.demote(4)).to eq(a1)
+      expect(a1.demote!(4)).to eq(a1)
     end
   end
 
   describe '#denillify(!)' do
+    let(:a1) { [nil, 2, 3] }
+
     it 'to be [0, 2, 3]' do
-      expect([nil, 2, 3].denillify).to eq([0, 2, 3])
-      expect([nil, 2, 3].denillify!).to eq([0, 2, 3])
+      a2 = [0, 2, 3]
+
+      expect(a1.denillify).to eq(a2)
+      expect(a1.denillify!).to eq(a2)
     end
 
     it 'to be [9, 2, 3]' do
-      expect([nil, 2, 3].denillify(9)).to eq([9, 2, 3])
-      expect([nil, 2, 3].denillify!(9)).to eq([9, 2, 3])
+      a2 = [9, 2, 3]
+
+      expect(a1.denillify(9)).to eq(a2)
+      expect(a1.denillify!(9)).to eq(a2)
     end
   end
 
   describe '#delete_first(!)' do
     it 'to be [2, 3]' do
-      expect([1, 2, 3].delete_first).to eq([2, 3])
-      expect([1, 2, 3].delete_first!).to eq([2, 3])
+      a1 = [1, 2, 3]
+      a2 = [2, 3]
+
+      expect(a1.delete_first).to eq(a2)
+      expect(a1.delete_first!).to eq(a2)
     end
   end
 
   describe '#delete_last(!)' do
     it 'to be [1, 2]' do
-      expect([1, 2, 3].delete_last).to eq([1, 2])
-      expect([1, 2, 3].delete_last!).to eq([1, 2])
+      a1 = [1, 2, 3]
+      a2 = [1, 2]
+
+      expect(a1.delete_last).to eq(a2)
+      expect(a1.delete_last!).to eq(a2)
     end
   end
 
   describe '#delete_values' do
     it 'to be [3, 4]' do
-      array = [1, 2, 3, 4]
-      array.delete_values(1, 2)
+      a1 = [1, 2, 3, 4]
+      a1.delete_values(1, 2)
 
-      expect(array).to eq([3, 4])
+      expect(a1).to eq([3, 4])
     end
   end
 
@@ -115,31 +138,35 @@ RSpec.describe Array do
   end
 
   describe '#from' do
+    a1 = [1, 2, 3]
+
     it 'to be [1, 2, 3]' do
-      expect([1, 2, 3].from(0)).to eq([1, 2, 3])
+      expect(a1.from(0)).to eq(a1)
     end
 
     it 'to be [2, 3]' do
-      expect([1, 2, 3].from(1)).to eq([2, 3])
+      expect(a1.from(1)).to eq([2, 3])
     end
 
     it 'to be [3]' do
-      expect([1, 2, 3].from(-1)).to eq([3])
+      expect(a1.from(-1)).to eq([3])
     end
   end
 
   describe '#fulfill' do
+    let(:a1) { [1, 2] }
+
     it 'to be [1, 2, "x", "x"]' do
-      expect([1, 2].fulfill('x', 4)).to eq([1, 2, 'x', 'x'])
+      expect(a1.fulfill('x', 4)).to eq([1, 2, 'x', 'x'])
     end
 
     it 'to be [1, 2, "x"]' do
-      expect([1, 2].fulfill('x', 3)).to eq([1, 2, 'x'])
+      expect(a1.fulfill('x', 3)).to eq([1, 2, 'x'])
     end
 
     it 'to be [1, 2]' do
-      expect([1, 2].fulfill('x', 2)).to eq([1, 2])
-      expect([1, 2].fulfill('x', 1)).to eq([1, 2])
+      expect(a1.fulfill('x', 2)).to eq(a1)
+      expect(a1.fulfill('x', 1)).to eq(a1)
     end
   end
 
@@ -150,8 +177,10 @@ RSpec.describe Array do
   end
 
   describe '#in_groups' do
+    a1 = %w[1 2 3 4 5 6 7 8 9 10]
+
     it 'to be [["1", "2", "3", "4"], ["5", "6", "7", nil], ["8", "9", "10", nil]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups(3)).to eq([
+      expect(a1.in_groups(3)).to eq([
                                                             %w[1 2 3 4],
                                                             ['5', '6', '7', nil],
                                                             ['8', '9', '10', nil]
@@ -159,7 +188,7 @@ RSpec.describe Array do
     end
 
     it 'to be [["1", "2", "3", "4"], ["5", "6", "7", "&nbsp;"], ["8", "9", "10", "&nbsp;"]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups(3, '&nbsp;')).to eq([
+      expect(a1.in_groups(3, '&nbsp;')).to eq([
                                                                       %w[1 2 3 4],
                                                                       ['5', '6', '7', '&nbsp;'],
                                                                       ['8', '9', '10', '&nbsp;']
@@ -167,7 +196,7 @@ RSpec.describe Array do
     end
 
     it 'to be [["1", "2", "3", "4"], ["5", "6", "7"], ["8", "9", "10"]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups(3, false)).to eq([
+      expect(a1.in_groups(3, false)).to eq([
                                                                    %w[1 2 3 4],
                                                                    %w[5 6 7],
                                                                    %w[8 9 10]
@@ -176,17 +205,14 @@ RSpec.describe Array do
   end
 
   describe '#in_groups_of' do
+    a1 = %w[1 2 3 4 5 6 7 8 9 10]
+
     it 'to be [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["10", nil, nil]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups_of(3)).to eq([
-                                                               %w[1 2 3],
-                                                               %w[4 5 6],
-                                                               %w[7 8 9],
-                                                               ['10', nil, nil]
-                                                             ])
+      expect(a1.in_groups_of(3)).to eq([%w[1 2 3], %w[4 5 6], %w[7 8 9], ['10', nil, nil]])
     end
 
     it 'to be [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["10", "&nbsp;", "&nbsp;"]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups_of(3, '&nbsp;')).to eq([
+      expect(a1.in_groups_of(3, '&nbsp;')).to eq([
                                                                          %w[1 2 3],
                                                                          %w[4 5 6],
                                                                          %w[7 8 9],
@@ -195,7 +221,7 @@ RSpec.describe Array do
     end
 
     it 'to be [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["10"]]' do
-      expect(%w[1 2 3 4 5 6 7 8 9 10].in_groups_of(3, false)).to eq([
+      expect(a1.in_groups_of(3, false)).to eq([
                                                                       %w[1 2 3],
                                                                       %w[4 5 6],
                                                                       %w[7 8 9],
@@ -212,18 +238,23 @@ RSpec.describe Array do
 
   describe '#merge(!)' do
     it 'to be [0, 1, 2, 3, 4, 5]' do
-      arr_one = [2, 3]
-      arr_two = [4, 5]
+      a1 = [0, 1]
+      a2 = [2, 3]
+      a3 = [4, 5]
+      a4 = [0, 1, 2, 3, 4, 5]
 
-      expect([0, 1].merge(arr_one, arr_two)).to eq([0, 1, 2, 3, 4, 5])
-      expect([0, 1].merge!(arr_one, arr_two)).to eq([0, 1, 2, 3, 4, 5])
+      expect(a1.merge(a2, a3)).to eq(a4)
+      expect(a1.merge!(a2, a3)).to eq(a4)
     end
   end
 
   describe '#nillify(!)' do
     it 'to be [nil, 2, 3]' do
-      expect(['', 2, 3].nillify).to eq([nil, 2, 3])
-      expect(['', 2, 3].nillify!).to eq([nil, 2, 3])
+      a1 = ['', 2, 3]
+      a2 = [nil, 2, 3]
+
+      expect(a1.nillify).to eq(a2)
+      expect(a1.nillify!).to eq(a2)
     end
   end
 
@@ -246,19 +277,25 @@ RSpec.describe Array do
   end
 
   describe '#promote' do
+    let(:a1) { [1, 2, 2, 3] }
+
     it 'to be [2, 2, 1, 3]' do
-      expect([1, 2, 2, 3].promote(2)).to eq([2, 2, 1, 3])
-      expect([1, 2, 2, 3].promote!(2)).to eq([2, 2, 1, 3])
+      a2 = [2, 2, 1, 3]
+
+      expect(a1.promote(2)).to eq(a2)
+      expect(a1.promote!(2)).to eq(a2)
     end
 
     it 'to be [3, 1, 2, 2]' do
-      expect([1, 2, 2, 3].promote(3)).to eq([3, 1, 2, 2])
-      expect([1, 2, 2, 3].promote!(3)).to eq([3, 1, 2, 2])
+      a2 = [3, 1, 2, 2]
+
+      expect(a1.promote(3)).to eq(a2)
+      expect(a1.promote!(3)).to eq(a2)
     end
 
     it 'to be [1, 2, 2, 3]' do
-      expect([1, 2, 2, 3].promote(4)).to eq([1, 2, 2, 3])
-      expect([1, 2, 2, 3].promote!(4)).to eq([1, 2, 2, 3])
+      expect(a1.promote(4)).to eq(a1)
+      expect(a1.promote!(4)).to eq(a1)
     end
   end
 
@@ -276,9 +313,9 @@ RSpec.describe Array do
 
   describe '#sample!' do
     it 'to be true' do
-      sarray = [1, 2, 3]
+      a1 = [1, 2, 3]
 
-      expect(sarray.exclude?(sarray.sample!)).to eq(true)
+      expect(a1.exclude?(a1.sample!)).to eq(true)
     end
   end
 
@@ -294,37 +331,50 @@ RSpec.describe Array do
 
   describe '#strip(!)' do
     it 'to be ["this", "is", "a", "test"]' do
-      expect('this    is   a  test'.split(' ').strip).to eq(%w[this is a test])
-      expect('this    is   a  test'.split(' ').strip!).to eq(%w[this is a test])
+      a1 = 'this    is   a  test'.split(' ')
+      a2 = %w[this is a test]
+
+      expect(a1.strip).to eq(a2)
+      expect(a1.strip!).to eq(a2)
     end
 
     it 'to be ["this", "that"]' do
-      expect(['this', '', 'that', nil, false].strip).to eq(%w[this that])
-      expect(['this', '', 'that', nil, false].strip!).to eq(%w[this that])
+      a1 = ['this', '', 'that', nil, false]
+      a2 = %w[this that]
+
+      expect(a1.strip).to eq(a2)
+      expect(a1.strip!).to eq(a2)
     end
   end
 
   describe '#swap' do
     it 'to be [3, 2, 1]' do
-      expect([1, 2, 3].swap(0, 2)).to eq([3, 2, 1])
+      a1 = [1, 2, 3]
+
+      expect(a1.swap(0, 2)).to eq([3, 2, 1])
     end
   end
 
   describe '#to' do
+    a1 = [1, 2, 3]
+
     it 'to be [1]' do
-      expect([1, 2, 3].to(0)).to eq([1])
+      expect(a1.to(0)).to eq([1])
     end
 
     it 'to be [1, 2]' do
-      expect([1, 2, 3].to(1)).to eq([1, 2])
+      expect(a1.to(1)).to eq([1, 2])
     end
 
     it 'to be [1, 2, 3]' do
-      expect([1, 2, 3].to(-1)).to eq([1, 2, 3])
+      expect(a1.to(-1)).to eq(a1)
     end
   end
 
   describe '#to_sentence' do
+    a1 = %w[one two]
+    a2 = %w[one two three]
+
     it 'to be ""' do
       expect([].to_sentence).to eq('')
     end
@@ -334,25 +384,24 @@ RSpec.describe Array do
     end
 
     it 'to be "one and two"' do
-      expect(%w[one two].to_sentence).to eq('one and two')
+      expect(a1.to_sentence).to eq('one and two')
     end
 
     it 'to be "one, two, and three"' do
-      expect(%w[one two three].to_sentence).to eq('one, two, and three')
+      expect(a2.to_sentence).to eq('one, two, and three')
     end
 
     it 'to be "one-two"' do
-      expect(%w[one two].to_sentence(two_words_connector: '-')).to eq('one-two')
+      expect(a1.to_sentence(two_words_connector: '-')).to eq('one-two')
     end
 
     it 'to be "one or two or at least three"' do
-      str = 'one or two or at least three'
-      results = %w[one two three].to_sentence(
+      s1 = a2.to_sentence(
         words_connector: ' or ',
         last_word_connector: ' or at least '
       )
 
-      expect(results).to eq(str)
+      expect(s1).to eq('one or two or at least three')
     end
   end
 
